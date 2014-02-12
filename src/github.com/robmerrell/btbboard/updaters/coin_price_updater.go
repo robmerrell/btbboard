82846799d6d020dfd7773689d48b902d41e18300
Rob Merrell <robmerrell@i.tv>
2014-02-11 23:51:41 -0700

    Use the cryptsy full market api to get the current prices, because calling the api for only the bitbar market is doesn't work.

--- src/github.com/robmerrell/btbboard/updaters/coin_price_updater.go
+++ src/github.com/robmerrell/btbboard/updaters/coin_price_updater.go
@@ -10,11 +10,13 @@ import (
 )
 
 var coinbaseUrl = "https://coinbase.com/api/v1/currencies/exchange_rates"
-var cryptsyUrl = "http://pubapi.cryptsy.com/api.php?method=singlemarketdata&marketid=151"
+
+// the cryptsy API is currenty broken for BTB, so get prices from the full market data.
+var cryptsyUrl = "http://pubapi.cryptsy.com/api.php?method=marketdatav2"
 
 type CoinPrice struct{}
 
-// Update retrieves VTC buy prices in both USD and BTC and saves
+// Update retrieves BTB buy prices in both USD and BTC and saves
 // the prices to the database.
 func (c *CoinPrice) Update() error {
 	usd, err := coinbaseQuote()
@@ -61,7 +63,7 @@ func coinbaseQuote() (float64, error) {
 	return strconv.ParseFloat(value.Btc, 64)
 }
 
-// cryptsyQuote gets the current VTC/BTC value from cryptsy.
+// cryptsyQuote gets the current BTB/BTC value from cryptsy.
 func cryptsyQuote() (float64, error) {
 	resp, err := http.Get(cryptsyUrl)
 	if err != nil {
@@ -72,16 +74,16 @@ func cryptsyQuote() (float64, error) {
 
 	var value struct {
 		Return struct {
-			Markets struct {
-				VTC struct {
-					RecentTrades []map[string]string
-				}
-			}
+			Markets map[string]interface{}
 		}
 	}
 	if err := json.Unmarshal(body, &value); err != nil {
 		return 0.0, err
 	}
 
-	return strconv.ParseFloat(value.Return.Markets.VTC.RecentTrades[0]["price"], 64)
+	market := value.Return.Markets["BTB/BTC"].(map[string]interface{})
+	recentTrades := market["recenttrades"].([]interface{})
+	price := recentTrades[0].(map[string]interface{})["price"].(string)
+
+	return strconv.ParseFloat(price, 64)
 }
